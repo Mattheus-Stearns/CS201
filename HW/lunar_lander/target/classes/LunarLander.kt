f68969e9c811bb0e380b451266c90bf3ba5e1163
Mattheus Stearns <mattheuspstearns@gmail.com>
2025-09-18 18:17:23 -0500

    W1HW First Objective

--- HW/lunar_lander/target/classes/LunarLander.kt
+++ HW/lunar_lander/target/classes/LunarLander.kt
@@ -1,26 +1,67 @@
 /*
  * Author: Mattheus Stearns
  * 
- * Collaboration statement: TODO
+ * Bibliography: https://kotlinlang.org/docs/home.html?s=full
+ * 
+ * Collaboration statement: N/A
  */
+
+import java.io.File
 
 // Initializing a class with instance variables as my constructor variables
 class LunarLander (var altitude: Int = 1000, var velocity: Int = 40, var fuel: Int = 25) {
     
+    private val logMessages: MutableList<String> = mutableListOf()
+
+    init {
+        logMessages.add("Initial Altitude: ${altitude}")
+        logMessages.add("Initial Velocity: ${velocity}")
+        logMessages.add("Initial Fuel: ${fuel}")
+    }
+
+
+
     // A Function that allows the user to burn their fuel
     fun burn (fuelRequested: Int) {
-        
+
+        logMessages.add("Requested Fuel Burn: ${fuelRequested}")
+        var fuelBurnt: Int = fuelRequested
+
         // Just preventing the user from burning more fuel than they have
-        if (fuelRequested > fuel) {
-            fuel = 0
-        } else {
-            fuel -= fuelRequested
+        if (fuelBurnt > fuel) {
+            fuelBurnt = fuel
         }
 
-        velocity -= (4 * fuel)
+        fuel -= fuelBurnt
+        logMessages.add("Resultant Fuel: ${fuel}")
+
+        velocity -= (4 * fuelBurnt)
+        logMessages.add("Resultant Velocity after Fuel burn: ${velocity}")
+
         velocity += 2
+        logMessages.add("Resultant Velocity after gravity's effect: ${velocity}")
 
         altitude -= velocity
+        logMessages.add("Resultant Altitude: ${altitude}")
+
+        // This is for logging purposes so that I can easier pass the tests
+        val fileName = "Log.txt"
+        
+        try {
+            File(fileName).writeText("=========================\n\n     Log     Message\n\n=========================\n\n")
+            } catch (e: Exception) {
+                println("An error occurred: ${e.message}")
+            }
+
+        for (message in logMessages) {
+            try {
+            File(fileName).appendText(message + "\n")
+            } catch (e: Exception) {
+                println("An error occurred: ${e.message}")
+            }
+        }
+
+        
     }
 
     // A Function that checks on the status of the spaceship
@@ -33,11 +74,12 @@ class LunarLander (var altitude: Int = 1000, var velocity: Int = 40, var fuel: I
         }
 
     }
+
+
 }
 
 
 fun main() {
-
 
 }
 
